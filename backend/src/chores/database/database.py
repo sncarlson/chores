7728af065df72caf4ce90305c9df5d4f9a19a2c4
Steven Carlson <sncarlson@dev-hive.org>
2020-08-08 10:56:42 -0400

    Updated DB

--- backend/src/chores/database/database.py
+++ backend/src/chores/database/database.py
@@ -1,9 +1,8 @@
 # TODO Implement Database here
-from flask import current_app
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask import current_app as app
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-print(current_app.config['SQLALCHEMY_DATABASE_URI'])
