b73d310750ce9c5d2d4a50c0b00455db2b517cb5
Steven Carlson <sncarlson@dev-hive.org>
2020-08-09 19:34:31 -0400

    Started to implement tests

--- test_app.py
+++ test_app.py
@@ -4,3 +4,158 @@ TODO Includes at least one test for expected success and error behavior for each
 
 TODO Includes tests demonstrating role-based access control, at least two per role.
 """
+
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app
+from model.models import Chore, Area, Worker, AssignedChores
+
+
+class ChoresTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "chores_test"
+        self.database_path = "postgres://{}/{}".format('chores:chores@localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        pass
+
+
+"""
+class TriviaTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format('trivia:trivia@hive:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        pass
+
+
+
+    def test_get_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_get_questions(self):
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+
+    def test_get_questions_paginated(self):
+        response = self.client().get('/questions?page=1')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+
+    def test_get_questions_paginated_past_page_bounds(self):
+        response = self.client().get('/questions?page=6')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+    def test_add_question(self):
+        response = self.client().post('/questions',
+                                      json={'question': 'Test Question', 'answer': 'Test Answer', 'category': '1',
+                                            'difficulty': '1'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_add_bad_question(self):
+        response = self.client().post('/questions', json={'question': '', 'answer': 'Test Answer', 'category': '1',
+                                                          'difficulty': '1'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+
+    def test_delete_question(self):
+        question = Question.query.filter(Question.question.ilike("%" + "Test Question" + "%")).first()
+
+        response = self.client().delete('/questions/' + str(question.id))
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_delete_question_failure(self):
+        response = self.client().delete('/questions/5000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+    def test_search_questions(self):
+        response = self.client().post('/questions/search', json={'searchTerm': 'paintings'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+
+    def test_search_questions_not_found(self):
+        response = self.client().post('/questions/search', json={'searchTerm': 'pizza'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+    def test_search_questions_by_category(self):
+        response = self.client().get('/categories/1/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['current_category'])
+
+    def test_search_questions_by_category_with_bad_category(self):
+        response = self.client().get('/categories/100/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+"""
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
