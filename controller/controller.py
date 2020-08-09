e2beee95b6dda31ad604987f8cd1fba6d08bb35a
Steven Carlson <sncarlson@dev-hive.org>
2020-08-09 19:17:29 -0400

    Updated endpoints

--- controller/controller.py
+++ controller/controller.py
@@ -8,8 +8,8 @@ TODO Specifies endpoints and behavior for at least:
     for at least four different status codes
 """
 
-from flask import Blueprint
-from model.models import Chore, Area, Worker, AssignedChores
+from flask import Blueprint, jsonify
+from model.models import Area
 
 # Blueprint Configuration
 controller_bp = Blueprint(
@@ -17,6 +17,89 @@ controller_bp = Blueprint(
 )
 
 
-@controller_bp.route('/', methods=['GET'])
-def home():
+@controller_bp.route('/chores', methods=['GET'])
+def chores():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/chores', methods=['POST'])
+def create_chores():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/chores/<chore_id>', methods=['DELETE'])
+def delete_chore(chore_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/chores/<chore_id>', methods=['PATCH'])
+def update_chore(chore_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/areas', methods=['GET'])
+def areas():
+    all_areas = Area.query.all()
+
+    data = [area.format() for area in all_areas]
+
+    result = {
+        "success": True,
+        "areas": data
+    }
+    return jsonify(result)
+
+
+@controller_bp.route('/areas', methods=['POST'])
+def create_area():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/areas/<area_id>', methods=['DELETE'])
+def delete_area(area_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/areas/<area_id>', methods=['PATCH'])
+def update_area(area_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/workers', methods=['GET'])
+def workers():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/workers', methods=['POST'])
+def create_worker():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/workers/<worker_id>', methods=['DELETE'])
+def delete_worker(worker_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/workers/<worker_id>', methods=['PATCH'])
+def update_worker(worker_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/assigned-chores', methods=['GET'])
+def assigned_chores():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/assigned-chores', methods=['POST'])
+def assign_chore():
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/assigned-chores/<assigned_chore_id>', methods=['DELETE'])
+def delete_assigned_chore(assigned_chore_id):
+    return "Hello from the controller!"
+
+
+@controller_bp.route('/assigned-chores/<assigned_chore_id>', methods=['PATCH'])
+def update_assigned_chore(assigned_chore_id):
     return "Hello from the controller!"
