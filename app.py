1d130d3a6f56b2a756cdef28ca0e57ca39906932
Steven Carlson <sncarlson@dev-hive.org>
2020-08-22 11:31:19 -0400

    Changed app

--- app.py
+++ app.py
@@ -21,7 +21,4 @@ def create_app():
     return app
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app = create_app()
-    app.run(host='0.0.0.0', port=port)
+app = create_app()
